b274c092b3f67a1e809f82071b1f193b193dc5d2
Zhuyaqiang <zyq1512099831@gmail.com>
2020-03-14 22:55:13 +0800

    springmvc

---
+++ springmvc-03-annotation/src/main/java/com/zyq/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.zyq.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/h1")
+    public String Hello(Model model) {
+        // 封装数据
+        model.addAttribute("msg", "Hello, SpringMVCAnnotation!");
+        return "hello"; // 会被视图解析器处理
+    }
+}
